35cb39dcb33222482169b0a9b58a1e1c1f917fc1
Kippei Watanabe <k6play7210@gmail.com>
2020-10-29 21:26:22 +0900

    #1 picture表示

--- api/frontend/src/App.js
+++ api/frontend/src/App.js
@@ -3,12 +3,15 @@ import axios from "axios";
 
 class App extends Component {
   state = {
-    news: []
+    news: [],
+    picture: []
   };
-  
+
   componentDidMount() {
     this.getNews();
+    this.getPicture();
   }
+
 
   getNews() {
     axios
@@ -23,17 +26,40 @@ class App extends Component {
       });
   }
 
+  getPicture() {
+    axios
+      .get("http://localhost:8000/api/picture", {
+        withCredentials: true
+      })
+      .then(res => {
+        this.setState({ picture: res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
+        <div id="news">
         {this.state.news.map(item => (
-          <div>
-            <h2>「{item.title}」</h2>
-            <p>   -{item.label}</p>
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>-{item.label}</p>
             <br/>
             <p>{item.content}</p>
           </div>
         ))}
+        </div>
+        <div id="picture">
+        {this.state.picture.map(item => (
+          <div>
+          <h3>{item.title}</h3>
+          <img src={item.picture} alt="" width="300"/>
+          </div>
+        ))}
+        </div>
       </div>
     )
   }
